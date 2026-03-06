c071ee91c53a80208d6c097fac9e93e7d3a9de3b
527adwarampudi-design <527adwarampudi@frhsd.com>
2026-03-06 08:46:34 -0500

    Day 1 changes

---
+++ apps.js
@@ -0,0 +1,14 @@
+let wordBank = [
+  "cupcake",
+  "brownie",
+  "donut",
+  "cheesecake",
+  "macaron",
+  "pudding",
+  "waffle",
+  "cookie",
+  "icecream",
+  "tiramisu",
+  "croissant",
+  "cannoli"
+];
